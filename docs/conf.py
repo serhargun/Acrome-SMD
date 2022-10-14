cf066593c4b50573fadb6c7ecf3ad53cbf2a8010
serhargun <serhanargun@gmail.com>
2022-10-14 15:38:19 +0300

    bat file updated

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,8 @@ import sys
 
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('./figures'))
-sys.path.insert(0, os.path.abspath('./exts')) # needed for fibre_autodoc extension
+sys.path.insert(0, os.path.abspath('./exts')) 
+
 project = 'Acrome Actuator Board'
 copyright = '2022, Acrome Robotics'
 author = 'Acrome Robotics'
@@ -26,10 +27,11 @@ extensions = [
     'sphinx.ext.autosummary', # Generate summary tables for Python documentation
     'sphinx.ext.intersphinx',  # Hyperlinks to external projects (such as Python standard library)
 ]
-templates_path = ['_templates']
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
+templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output
 html_theme = 'sphinx_rtd_theme'
@@ -38,6 +40,7 @@ html_theme_options = {
     'style_external_links': True,
     'display_version': True,
 }
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
