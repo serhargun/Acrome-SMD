32c494482d8c0f9572428a494332bd5825e79e92
Serhan Argun <30629499+serhargun@users.noreply.github.com>
2023-02-03 18:32:21 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('./figures'))
 sys.path.insert(0, os.path.abspath('./exts')) 
 
-project = 'Acrome Actuator Board'
+project = 'Acrome SMD'
 copyright = '2022, Acrome Robotics'
 author = 'Acrome Robotics'
 
